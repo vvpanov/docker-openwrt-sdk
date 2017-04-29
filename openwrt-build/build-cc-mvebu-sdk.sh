29d7cd080db2e8014c69290c00419c35e1f13805
Vadim P <vadim.v.panov@gmail.com>
2017-04-29 20:23:56 +0800

    update form upstream, allow case insensitive fs

--- openwrt-build/build-cc-mvebu-sdk.sh
+++ openwrt-build/build-cc-mvebu-sdk.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-docker run --rm -it -v $(pwd)/out-cc:/opt/openwrt vpanov/openwrt-build  sh -c "\
+docker run --rm -it \
+	-v $(pwd)/out-cc:/opt/openwrt \
+	-v $(pwd)/patches:/opt/patches \
+	-v $(pwd)/dl:/opt/openwrt-build/dl \
+		vpanov/openwrt-build  sh -c "\
 	cd /opt && (test -d openwrt/.git ||  git clone -b chaos_calmer git://github.com/openwrt/openwrt.git) && \
 	\
 	echo 'CONFIG_TARGET_mvebu=y'                >  /opt/openwrt/.config && \
@@ -15,9 +19,19 @@ docker run --rm -it -v $(pwd)/out-cc:/opt/openwrt vpanov/openwrt-build  sh -c "\
 	echo 'CONFIG_PER_FEED_REPO_ADD_COMMENTED=n' >> /opt/openwrt/.config && \
 	\
 	cd /opt/openwrt && \
+	git checkout c170d84 && \
 	./scripts/feeds update && \
 	./scripts/feeds install luaposix luasocket luci patch qrencode && \
+	\
+	tar c * .* | tar xv -C /opt/openwrt-build && \
+	cd /opt/openwrt-build && \
+	git config --global user.email dummy@dummy.org && \
+	git config --global user.name dummy && \
+	git cherry-pick ca77544 && \
+	cat /opt/patches/* | patch -p1 && \
+	\
 	make defconfig && make V=$V && \
 	\
+	tar cvf /opt/openwrt/build-mvebu.tar /opt/openwrt-build/bin/mvebu && \
 	echo OK!\
 "
